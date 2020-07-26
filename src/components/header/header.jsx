4fa264a5646b6d028f3546cca7c4edd87f75fd9b
snstrauss <zahavi.guy.85@gmail.com>
2020-07-26 13:00:08 +0300

    header buttons are now built dynamically from 'views' export

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,19 @@
 import React, { useContext } from 'react';
 import S from './header.module.scss';
 import { EnvContext } from '../../pages/index';
+import views from '../../views';
 
 export default function Header(){
 
     const { isBigScreen } = useContext(EnvContext);
-
-    const links = [
-        {
-            display: '<Guy/>',
-            link: '#hero'
-        },
-        {
-            link: '#about'
-        },
-        {
-            link: 'pdf link'
-        },
-        {
-            link: '#projects'
-        }
-    ]
 
     const navClassName = `${S.container} ${isBigScreen ? S.onBigScreen : ''}`;
 
     return (
         <nav className={navClassName}>
             {
-                links.map((link) => (
-                    <a key={link.link} href={link.link}>{link.display ? link.display : link.link.replace('#', '')}</a>
+                Object.keys(views).map((tab, idx) => (
+                    <a key={tab} href={`#${idx}`}>{tab.replace('Hero', '<Guy/>')}</a>
                 ))
             }
         </nav>
