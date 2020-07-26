1b665faa0e3570fd1ca4e1da2e16054ee4ceefb1
snstrauss <zahavi.guy.85@gmail.com>
2020-07-26 12:59:33 +0300

    created slugs for mock tabs - just to test the dynamic header and scoller

---
+++ src/views/blam/blam.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import S from './blam.module.scss';
+
+export default function Blam(){
+
+    return (
+        <div className={S.container}>
+            <h2>BLAM</h2>
+        </div>
+    )
+}
---
+++ src/views/stam/stam.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import S from './stam.module.scss';
+
+export default function Stam(){
+
+    return (
+        <div className={S.container}>
+            <h2>STAM</h2>
+        </div>
+    )
+}
