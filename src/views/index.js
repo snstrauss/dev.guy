becf898c07ddd331ff965f9c6839215dfea66d59
snstrauss <zahavi.guy.85@gmail.com>
2020-07-26 13:01:05 +0300

    export 'views' tabs

---
+++ src/views/index.js
@@ -0,0 +1,19 @@
+
+import Hero from './hero/hero';
+import About from './about/about';
+import CV from './cv/cv';
+import Projects from './projects/projects';
+
+
+import Stam from './stam/stam';
+import Blam from './blam/blam';
+
+export default {
+    Hero,
+    About,
+    CV,
+    Projects,
+
+    Stam,
+    Blam
+}
