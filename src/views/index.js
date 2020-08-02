0fdf3b5e9060f583750c9430864f7559660cd6bd
snstrauss <zahavi.guy.85@gmail.com>
2020-08-02 14:32:24 +0300

    remove test views

--- src/views/index.js
+++ src/views/index.js
@@ -4,16 +4,9 @@ import About from './about/about';
 import CV from './cv/cv';
 import Projects from './projects/projects';
 
-
-import Stam from './stam/stam';
-import Blam from './blam/blam';
-
 export default {
     Hero,
     About,
     CV,
-    Projects,
-
-    Stam,
-    Blam
+    Projects
 }
